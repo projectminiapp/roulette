549807c0c5ab4b405648bae26022a72754bdfcef
Diego <diegorandal@gmail.com>
2025-09-11 23:30:33 -0300

    Reiniciando el repositorio

--- app/api/nonce/route.ts
+++
@@ -1,7 +0,0 @@
-// /app/api/nonce/route.ts
-import { NextResponse } from "next/server"
-
-export async function GET() {
-  const nonce = crypto.randomUUID()
-  return NextResponse.json({ nonce })
-}
